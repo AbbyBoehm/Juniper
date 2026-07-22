43372e308e3dd52eae940bc594387c078f3056d8
Abby Boehm <vab55@cornell.edu>
2026-07-22 14:35:00 -0400

    fixed init

--- juniper/stage4/__init__.py
+++ juniper/stage4/__init__.py
@@ -3,8 +3,8 @@ __all__ = ["do_stage4",
            "extract_photseries",
            "align_spec",
            "decorrelate_photseries",
-           "clean_spec",
-           "plot_spec_gif",]
+           "clean_signal",
+           "plot_signal_gif",]
 
 from juniper.stage4.do_stage4 import do_stage4
-from juniper.stage4 import extract_1D, extract_photseries, align_spec, decorrelate_photseries, clean_spec, plot_spec_gif
+from juniper.stage4 import clean_signal, extract_1D, extract_photseries, align_spec, decorrelate_photseries, plot_signal_gif
